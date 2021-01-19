ef2aecc7c64dc991c16ef7ef255aad18921baaff
jessiek208 <73068180+jessiek208@users.noreply.github.com>
2021-01-19 16:12:03 -0500

    Initial Upload of Project 3

---
+++ server.js
@@ -0,0 +1,41 @@
+// Setup empty JS object to act as endpoint for all routes
+const projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+
+// Start up an instance of app
+const app = express();
+
+/* Middleware*/
+const bodyParser = require('body-parser')
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+function listening(){
+    console.log(`Server listening. Running on ${port}`);
+}
+
+app.get('/all', function (req, res) {
+    res.send(projectData);
+  });
+
+app.post('/add', addData);
+
+function addData(req, res){
+    projectData.date = req.body.date;
+    projectData.temp = req.body.temp;
+    projectData.journalEntry = req.body.content;
+    res.send(projectData);
+}
---
+++ website/app.js
@@ -0,0 +1,75 @@
+/* Global Variables */
+
+//const { request } = require("http");
+
+// Create a new date instance dynamically with JS
+let d = new Date(); 
+let newDate = (d.getMonth()+1)+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+//Access Weather API with Fetch
+let baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+let apiKey = ',us&appid=2af1d51a7c755c0ad75becae4e91561f';
+
+
+document.getElementById('generate').addEventListener('click', performAction);
+//when generate is clicked, execute performAction function below
+
+//takes zipcode and uses it in the getTepm function below
+function performAction(e){
+    const userContent = document.getElementById('feelings').value;
+    const zipcode = document.getElementById('zip').value;
+    getTemp(baseURL, zipcode, apiKey)
+    .then(function(data){
+        console.log(data);
+        const tempKelvin = data.main.temp;
+        const farenheitTemp = Math.round((1.8 * (tempKelvin - 273)) + 32);
+        postEntry('/add', {date: newDate, temp: farenheitTemp, content: userContent});
+        updateUI();
+    });
+};
+
+
+//function takes zipcode and fetches data from the API using created link
+const getTemp = async (baseURL, zipcode, apiKey) => {
+    const res = await fetch (baseURL+zipcode+apiKey);
+    try {
+        const data = await res.json();
+        //console.log(data);
+        return data;
+    }   catch(error) {
+        console.log("error", error);
+    }
+};
+
+//POST request to server    
+const postEntry = async ( url = '', data = {})=>{
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+    try {
+      const newData = await response.json();
+      console.log(newData);
+      return newData;
+    }catch(error) {
+    console.log(error);
+    }
+};
+
+//updates UI by pulling data and inserting it into divs
+const updateUI = async () => {
+    const request = await fetch ('/all');
+    try {
+        const allData = await request.json();
+        console.log(allData);
+        document.getElementById('date').innerHTML = "Date: " + allData.date;
+        document.getElementById('temp').innerHTML = "Temperature: " + allData.temp + " F";
+        document.getElementById('content').innerHTML = "Journal Entry: " + allData.journalEntry;
+    } catch(error) {
+        console.log(error);
+    }
+};
